0702403eaa9d737a659cda0036039b2df8da3602
Ilya <80645605+CookSoap@users.noreply.github.com>
2023-05-12 15:25:02 +0300

    first commit

---
+++ generate-text.php
@@ -0,0 +1,22 @@
+<?php
+include_once './markov.php';
+
+$markov = new Markov();
+
+$markov->addStates([
+    'Today is sunny',
+    'Today is rainy',
+    'Today is not rainy',
+    'The weather is sunny',
+    'The weather for today is sunny',
+    'The weather for tomorrow might be rainy',
+]);
+
+
+$markov->train();
+
+// Generate an output
+$text = $markov->generateRandom(30);
+echo $text;
+
+?>
